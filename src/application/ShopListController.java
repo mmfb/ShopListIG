188030dd4ceacac5c0e0949b88385b34b6d78146
Miguel <miguel.bugalho@gmail.com>
2019-10-07 15:21:09 +0100

    Added elements to the list
    Added elements to the list in the controller
    Added an ID to the ListView in the FXML

--- src/application/ShopListController.java
+++ src/application/ShopListController.java
@@ -1,5 +1,24 @@
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class ShopListController {
-	
+    @FXML
+    private ListView<String> shopListLV;
+    
+    private ObservableList<String> shopList =
+    		FXCollections.observableArrayList();
+    
+    @FXML
+    private void initialize() {
+    	shopListLV.setItems(shopList);
+    	shopList.add("Batata");
+    	shopList.add("Cebola");
+    	shopList.add("Pão");
+    	
+    }
+    
 }
