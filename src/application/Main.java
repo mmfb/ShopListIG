247fee099e4754fed320939309cf0000e46d7c76
Miguel <miguel.bugalho@gmail.com>
2019-10-07 15:01:51 +0100

    Added main window
    Window with shop list

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,8 +12,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader(
+					Main.class.getResource("ShopListView.fxml"));
+			loader.setController(new ShopListController());
+			Parent root = loader.load();			
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
