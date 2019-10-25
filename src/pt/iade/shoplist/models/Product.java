e638d2c7131234e33c78cf1c22876ea1a743a51d
Miguel <miguel.bugalho@gmail.com>
2019-10-25 16:14:34 +0100

    Added class product

---
+++ src/pt/iade/shoplist/models/Product.java
@@ -0,0 +1,20 @@
+package pt.iade.shoplist.models;
+
+public class Product {
+	protected String name;
+	protected double price;
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	@Override
+	public String toString() {
+		return name + " : " + price;
+	}
+}
