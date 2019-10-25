b7ad7d7ef83793fcd8d11574ad718bf249f5a8e0
Miguel <miguel.bugalho@gmail.com>
2019-10-25 18:25:00 +0100

    Added Item, Shoplist and ImportedProduct models
    Next steps:
     Create class that managem all data (ShoplistDAO)
     Create all controllers and views

---
+++ src/pt/iade/shoplist/models/ImportedProduct.java
@@ -0,0 +1,22 @@
+package pt.iade.shoplist.models;
+
+public class ImportedProduct extends Product {
+	protected String country;
+	protected String importer;
+	public ImportedProduct(String name, double price,
+			String country, String importer) {
+		super(name, price);
+		this.country = country;
+		this.importer = importer;
+	}
+	public String getCountry() {
+		return country;
+	}
+	@Override
+	public String toString() {
+		return "(" + country + ") " + super.toString();
+	}
+	 	
+	
+	
+}
---
+++ src/pt/iade/shoplist/models/Shoplist.java
@@ -0,0 +1,24 @@
+package pt.iade.shoplist.models;
+
+import javafx.collections.ObservableList;
+
+public class Shoplist {
+	private String name;
+	private ObservableList<Item> items;
+	public Shoplist(String name, ObservableList<Item> items) {
+		this.name = name;
+		this.items = items;
+	}
+	@Override
+	public String toString() {
+		return  name;
+	}
+	public double getCost() {
+		double sum =0;
+		for (Item item : items) {
+			sum += item.getCost();
+		}
+		return sum;
+	}
+	
+}
