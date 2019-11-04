bcb63d4ef0602b5aceb6408d92c8729212ef56c1
Miguel <miguel.bugalho@gmail.com>
2019-11-04 15:10:14 +0000

    Modal window and replace contents on window
    Changed WindowManager  openItemsWindow to replace the content instead of
    open a new window
       - Also in the code there is an example of opening a modal window
    Implemented the back button on the shoplist window
       - only makes sense in the case we use the same window

--- src/pt/iade/shoplist/WindowManager.java
+++ src/pt/iade/shoplist/WindowManager.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import pt.iade.shoplist.controllers.MainController;
 import pt.iade.shoplist.controllers.ShopListController;
@@ -24,16 +25,32 @@ final public class WindowManager {
 	static public void openItemsWindow(Shoplist shoplist) {
 		Parent root = createNodeTree("./views/ShopListView.fxml",
 				new ShopListController(shoplist));		
+		// Replace the content of the current window (primaryStage)
+		primaryStage.getScene().setRoot(root);
+		/*
+		// Open in a new window
 		Stage stage = new Stage();
+		
+		// Opens a modal window that blocks the owner
+		//stage.initOwner(primaryStage);
+		//stage.initModality(Modality.WINDOW_MODAL);
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(
 				Main.class.getResource("application.css").toExternalForm());
 		stage.setScene(scene);
 		stage.show();
+		*/
+	}
+	
+	// When we go back to main window
+	static public void backToMainWindow() {
+		Parent root = createNodeTree("./views/MainView.fxml",
+				new MainController());			
+		primaryStage.getScene().setRoot(root);	
 	}
 
 	
-	
+	// When we first open the window
 	static public void openMainWindow() {
 		Parent root = createNodeTree("./views/MainView.fxml",
 				new MainController());			
--- src/pt/iade/shoplist/controllers/ShopListController.java
+++ src/pt/iade/shoplist/controllers/ShopListController.java
@@ -3,6 +3,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import pt.iade.shoplist.WindowManager;
 import pt.iade.shoplist.models.Item;
 import pt.iade.shoplist.models.Shoplist;
 
@@ -27,6 +28,6 @@ public class ShopListController {
     
     @FXML
     private void backToMain(ActionEvent event) {
-    	
+    	WindowManager.backToMainWindow();
     }   
 }
